8be9222f49c5b65d992bae9cb37a566d1320b66b
EC2 Default User <ec2-user@ip-172-31-22-43.ap-northeast-1.compute.internal>
2023-08-16 10:30:44 +0000

    gem add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'user/registrations',
+    sessions:      'user/sessions',
+    passwords:     'user/passwords'
+  }
+  devise_for :admins, controllers: {
+    registrations: 'admin/registrations',
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
