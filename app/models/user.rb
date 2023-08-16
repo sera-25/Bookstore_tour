12d003835b48d6eeb2722bfd158441068fcfe984
EC2 Default User <ec2-user@ip-172-31-22-43.ap-northeast-1.compute.internal>
2023-08-16 20:52:14 +0000

    add model.controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRendom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
+ root to: 'users/homes#top'
+# ユーザー用
+  namespace :users do
+    root to: 'homes#top'
+  end
+  
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_"
+  end
+  
+  # 管理者用
+
   devise_for :users, controllers: {
-    registrations: 'user/registrations',
-    sessions:      'user/sessions',
-    passwords:     'user/passwords'
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+    passwords:     'users/passwords'
   }
   devise_for :admins, controllers: {
-    registrations: 'admin/registrations',
-    sessions:      'admin/sessions',
-    passwords:     'admin/passwords'
+    registrations: 'admins/registrations',
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
