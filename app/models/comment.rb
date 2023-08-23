44503b1192a336ab3822423117fa0659c0a206f4
EC2 Default User <ec2-user@ip-172-31-22-43.ap-northeast-1.compute.internal>
2023-08-23 18:27:31 +0000

    add comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+  
+  validates :comment, presence: true, langth: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :comments, dependent: :destroy
   
   def self.guest
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
