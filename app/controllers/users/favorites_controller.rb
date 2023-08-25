e0d481227c07a357c7424f46d20b51a2e034c02d
EC2 Default User <ec2-user@ip-172-31-22-43.ap-northeast-1.compute.internal>
2023-08-25 05:19:07 +0000

    add relationships

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,4 +1,6 @@
 class Users::FavoritesController < ApplicationController
+  
+  # お気に入り（いいね）するとき
   def create
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
@@ -6,6 +8,7 @@ class Users::FavoritesController < ApplicationController
     redirect_to request.referer
   end
   
+  # お気に入り（いいね）を外すとき
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
