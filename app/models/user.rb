7d83444aee4fc49532c50f6bf932b46f8f96e293
EC2 Default User <ec2-user@ip-172-31-22-43.ap-northeast-1.compute.internal>
2023-08-27 18:36:24 +0000

    add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
@@ -11,11 +12,31 @@ class User < ApplicationRecord
 
   has_many :following_users, through: :followers, source: :followed
   has_many :follower_users, through: :followeds, source: :follower
-  
+
+  has_one_attached :profile_image
+
+  def get_profile_image
+
+      (profile_image.attached?) ? profile_image : "no_image.png"
+  end
+
   def self.guest
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRendom.urlsafe_base64
       user.name = "guestuser"
     end
+  end
+ #　フォローしたときの処理
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+  
+  #　フォローを外すときの処理
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+  #フォローしていればtrueを返す
+  def following?(user)
+    following_users.include?(user)
   end
 end
